f6eede4dc09aa123b27df56b40f6f5e40e7e3085
Work <Work@MSI>
2023-11-30 09:55:14 -0600

    Where we ended on Nov 30 in the morning.

--- Chapter 8 - 1436-001/Source.cpp
+++ Chapter 8 - 1436-001/Source.cpp
@@ -1,10 +1,32 @@
 #include <iostream>
+#include<vector>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World";
+    //array == list
+
+    int theNumber = 1; //variable can vary
+    theNumber = 2;
+
+    
+    //make a list of strings (grocery list) 
+    vector<string> groceryList =
+    {
+        "eggs",
+        "bacon",
+        "milk", 
+        "something else"
+    };
+
+    //cout << groceryList[0] << endl; 
+    groceryList.push_back("peas");
+    //cout << groceryList.size() << endl; 
+    for (int i = 0; i < groceryList.size(); i++)
+    {
+        cout << groceryList[i] << endl; 
+    }
 
     return 0;
 }
